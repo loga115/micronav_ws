4db15dbfdb7c99de1920be237f6ac3375072924a
V16nesh <v16nesh2003@gmail.com>
2024-03-13 01:01:18 +0530

    serial com edited

--- install/serial_controller/lib/python3.10/site-packages/serial_controller/serial_node.py
+++ install/serial_controller/lib/python3.10/site-packages/serial_controller/serial_node.py
@@ -2,44 +2,32 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 import serial
-import time
 
-class SerialBiDirectional(Node):
+class SubscriberNode(Node):
     def __init__(self):
-        super().__init__('serial_bidirectional')
-        self.serial_port = '/dev/ttyS0'  # Update this with your actual serial port
-        self.serial_baudrate = 9600  # Update this with your ESP8266 baudrate
-        self.serial_connection = serial.Serial(self.serial_port, self.serial_baudrate)
-        time.sleep(2)  # Wait for the serial connection to be established
-        self.get_logger().info('Serial com established')
-        self.publisher_ = self.create_publisher(String, 'esp8266_data', 10)
+        super().__init__('subscriber_node')
         self.subscription = self.create_subscription(
             String,
-            'esp8266_commands',
-            self.receive_command_callback,
+            'data_topic',
+            self.listener_callback,
             10)
+        self.subscription  
+        self.serial_port = serial.Serial('/dev/ttyS0', 9600, timeout=1) 
 
-    def receive_command_callback(self, msg):
-        command = msg.data
-        self.get_logger().info('Received command: %s' % command)
-        self.serial_connection.write(command.encode())
+    def listener_callback(self, msg):
+        received_data = msg.data
+        self.get_logger().info('Received: "%s"' % received_data)
+        self.send_serial_data(received_data)
 
-    def publish_serial_data(self):
-        if self.serial_connection.is_open:
-            try:
-                line = self.serial_connection.readline().decode('utf-8').strip()
-                self.get_logger().info('Received data from ESP8266: %s' % line)
-                msg = String()
-                msg.data = line
-                self.publisher_.publish(msg)
-            except serial.SerialException as e:
-                self.get_logger().error('Serial connection error: %s' % str(e))
+    def send_serial_data(self, data):
+        data_to_send = data.encode()
+        self.serial_port.write(data_to_send)
 
 def main(args=None):
     rclpy.init(args=args)
-    serial_bidirectional = SerialBiDirectional()
-    rclpy.spin(serial_bidirectional)
-    serial_bidirectional.destroy_node()
+    subscriber_node = SubscriberNode()
+    rclpy.spin(subscriber_node)
+    subscriber_node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
